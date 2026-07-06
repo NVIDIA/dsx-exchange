95b56b19b5a1dde44549575e9a5ce301b6caec6c
Burak Serdar <bserdar@nvidia.com>
2026-07-06 09:40:43 -0600

    rename power-management to dsxflex
    Signed-off-by: Burak Serdar <bserdar@nvidia.com>

--- scripts/generate_asyncapi_docs.py
+++ scripts/generate_asyncapi_docs.py
@@ -29,7 +29,7 @@ DOCS = ROOT / "docs"
 
 SPECS = {
     "bms": ROOT / "schemas" / "asyncapi" / "bms" / "bms.yaml",
-    "power-management": ROOT / "schemas" / "asyncapi" / "power-management" / "power-management.yaml",
+    "dsxflex": ROOT / "schemas" / "asyncapi" / "dsxflex" / "dsxflex.yaml",
     "nico": ROOT / "schemas" / "asyncapi" / "nico" / "nico.yaml",
     "spiffe-exchange": ROOT / "schemas" / "asyncapi" / "spiffe-exchange" / "pub-keysets.yaml",
 }
